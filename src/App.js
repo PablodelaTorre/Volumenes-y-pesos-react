cc1a1c352ae52f8ec96a0a83c252d85dedd7f134
Pablo <delatorre.pablodaniel@gmail.com>
2022-04-11 00:10:12 -0300

    proyecto comenzado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import prismaRectangularRecto from './multimedia/prismaRectangularRecto.png'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar/>
+      <h1 className='titulo'>App</h1>
+      <div className='contenedor'>
+        <div className='tarjetas'>
+          <p>Prisma rectangular recto</p>
+          <div className='center'>
+            <img className='img-figuras' src={prismaRectangularRecto} alt="Prisma Rectangular Recto" />
+          </div>
+          <div className='center'>
+            <input type="text" name='largo' placeholder='Largo en cm'/>
+          </div>
+          <div className='center'>  
+            <input type="text" name='ancho' placeholder='Ancho en cm'/>
+          </div>
+          <div className='center'>
+            <input type="text" name='espesor' placeholder='Espesor en cm'/>
+          </div>
+          <div className='center'>
+            <input type="text" name='densidad' placeholder='Densidad en gramos/cm3'/>
+          </div>
+          <div className='center'>
+            <p className='resultado-paralelogramo'>Resultado:</p>
+          </div>
+          <div className='center'>  
+            <div className='resultado-volumen-paralelogramo'></div>
+          </div>
+          <div className='center'>  
+            <div className='resultado-peso-paralelogramo'></div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className='desplegable-navbar'>Figuras</div>
+        </div>
+    )
+}
+
+export default Navbar
