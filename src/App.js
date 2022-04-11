f41e4fa8032a151b2c348ef539ec5a0e7ab3ee78
Pablo <delatorre.pablodaniel@gmail.com>
2022-04-11 16:14:58 -0300

    primeras pruebas exitosas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import prismaRectangularRecto from './multimedia/prismaRectangularRecto.png'
 
 const App = () => {
+
+  const [largo, setLargo] = useState()
+  const [ancho, setAncho] = useState()
+  const [profundidad, setProfundidad] = useState()
+  const [densidad, setDensidad] = useState()
+  const [volumen, setVolumen] = useState()
+  const [peso, setPeso] = useState()
+
+  const handleChangeLargo = (e) => {
+    const value = e.target.value
+    setLargo(Number(value))
+  }
+
+  const handleChangeAncho = (e) => {
+    const value = e.target.value
+    setAncho(Number(value))
+  }
+
+  const handleChangeProfundidad = (e) => {
+    const value = e.target.value
+    setProfundidad(Number(value))
+  }
+
+  const handleChangeDensidad = (e) => {
+    const value = e.target.value
+    setDensidad(Number(value))
+  }
+
+
+  const volPrismaRecto = () => {
+    setVolumen(largo * ancho * profundidad)
+  }
+
+  const pesoPrismaRecto = () => {
+    setPeso(volumen * densidad)
+  }
+
   return (
     <>
       <Navbar/>
@@ -14,25 +51,31 @@ const App = () => {
             <img className='img-figuras' src={prismaRectangularRecto} alt="Prisma Rectangular Recto" />
           </div>
           <div className='center'>
-            <input type="text" name='largo' placeholder='Largo en cm'/>
-            <input type="text" name='ancho' placeholder='Ancho en cm'/>
+            <input onChange={handleChangeLargo} value={largo} type="text" name='largo' placeholder='Largo en cm'/>
+            <input onChange={handleChangeAncho} type="text" value={ancho} name='ancho' placeholder='Ancho en cm'/>
           </div>
           <div className='center'>
-            <input type="text" name='espesor' placeholder='Espesor en cm'/>
-            <input type="text" name='densidad' placeholder='Densidad en gramos/cm3'/>
+            <input onChange={handleChangeProfundidad} type="text" value={profundidad} name='profundidad' placeholder='Profundidad en cm'/>
+            <input onChange={handleChangeDensidad} type="text" value={densidad} name='densidad' placeholder='Densidad en gramos/cm3'/>
           </div>
           <p className='resultado-paralelogramo'>Resultados:</p>
           <div className='center'>
-            <p className='etiqueta-resultados'>Volumen:</p>  
+            <button className='calcular-prisma-rectangular-recto' onClick={volPrismaRecto}>Calcular volumen:</button>  
+          </div>
+          <div className='center'>
+            <p className='etiqueta-resultados'>Volumen en cm3:</p>  
           </div>
           <div className='center-etiquetas'>
-            <div className='resultado-volumen-paralelogramo'></div>
+            <div className='resultado-volumen-paralelogramo'>{volumen? volumen : ""}</div>
+          </div>
+          <div className='center'>
+            <button className='calcular-prisma-rectangular-recto' onClick={pesoPrismaRecto}>Calcular peso:</button>  
           </div>
           <div className='center-etiquetas'>  
-            <p className='etiqueta-resultados'>Peso:</p>
+            <p className='etiqueta-resultados'>Peso en gramos:</p>
           </div>
           <div className='center'>  
-            <div className='resultado-peso-paralelogramo'></div>
+            <div className='resultado-peso-paralelogramo'>{peso? peso : ""}</div>
           </div>
         </div>
       </div>
