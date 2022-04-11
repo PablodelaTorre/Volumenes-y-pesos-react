d84778d901dfdd96b393608820dbca623db6e70b
Pablo <delatorre.pablodaniel@gmail.com>
2022-04-11 00:33:57 -0300

    avances en estilos

--- src/App.js
+++ src/App.js
@@ -15,21 +15,21 @@ const App = () => {
           </div>
           <div className='center'>
             <input type="text" name='largo' placeholder='Largo en cm'/>
-          </div>
-          <div className='center'>  
             <input type="text" name='ancho' placeholder='Ancho en cm'/>
           </div>
           <div className='center'>
             <input type="text" name='espesor' placeholder='Espesor en cm'/>
-          </div>
-          <div className='center'>
             <input type="text" name='densidad' placeholder='Densidad en gramos/cm3'/>
           </div>
+          <p className='resultado-paralelogramo'>Resultados:</p>
           <div className='center'>
-            <p className='resultado-paralelogramo'>Resultado:</p>
+            <p className='etiqueta-resultados'>Volumen:</p>  
           </div>
-          <div className='center'>  
+          <div className='center-etiquetas'>
             <div className='resultado-volumen-paralelogramo'></div>
+          </div>
+          <div className='center-etiquetas'>  
+            <p className='etiqueta-resultados'>Peso:</p>
           </div>
           <div className='center'>  
             <div className='resultado-peso-paralelogramo'></div>
